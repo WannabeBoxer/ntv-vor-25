5ba00da4df614dc1915017fce02b796c874b6bf2
WannabeBoxer <steinar.ingolfs14@gmail.com>
2025-02-19 16:58:23 +0000

    horfði aftur á tíma 2

--- cheatsheet01.dart
+++ cheatsheet01.dart
@@ -5,15 +5,17 @@ void main(List<String> arguments){
   //Hér prentar hún út Steinar Ingólfsson
   String fullName = firstName + " " + lastName;
   print("Hi $fullName");
-
   String SSN = "140898-3259";
   String fixSSN = SSN.replaceAll("-","");
   //replaceALL er notað til þess að taka í burtu fyrri hlutann ss "-"
-  int ssnLength = SSN.length;
+  int ssnLength = fixSSN.length;
   print(ssnLength);
-  int nameLength = fullName.length;
-  print(nameLength);
+  String message = "Hello World";
+  String fixMessage = message.replaceAll("World", "NTV");
+  print(fixMessage.toUpperCase());
 
   String birthDate = SSN.substring(0,6);
   print(birthDate);
+  String email = "SteiNarING@NtV.is";
+  print(email.toLowerCase());
 }
